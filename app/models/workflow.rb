66db02626feb09784d21859a78b22ed0febd4ba4
Luke <imlukedewitt@gmail.com>
2025-05-06 20:00:17 -0500

    tidying up

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+##
+# Workflow model
 class Workflow < ApplicationRecord
   has_many :workflow_steps
 end
--- app/models/workflow_step.rb
+++ app/models/workflow_step.rb
@@ -1,8 +1,19 @@
+# frozen_string_literal: true
+
+##
+# Represents a step in a workflow sequence.
+# Each step has a specific order, configuration, and name.
+#
+# @attr_reader [String] name The name of the workflow step
+# @attr_reader [Hash] config The configuration settings for this step
+# @attr_reader [Integer] order The position of this step in the workflow sequence
+#
+# @attr [Workflow] workflow The workflow this step belongs to
 class WorkflowStep < ApplicationRecord
   belongs_to :workflow
   default_scope { order(order: :asc) }
 
   validates :config, presence: true
   validates :name, presence: true
-  validates :order, presence: true, numericality: { only_integer: true , greater_than: 0 }
+  validates :order, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
