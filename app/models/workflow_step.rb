7eb63ad96cd12aac7096751c7f77cf4724a46587
Luke <imlukedewitt@gmail.com>
2025-05-18 23:15:36 -0500

    add workflow step config validation and tests

--- app/models/workflow_step.rb
+++ app/models/workflow_step.rb
@@ -16,4 +16,47 @@ class WorkflowStep < ApplicationRecord
   validates :config, presence: true
   validates :name, presence: true
   validates :order, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  validate :validate_config
+
+  CONFIG_REQUIRED_KEYS = %w[
+    name
+    url
+  ].freeze
+  CONFIG_OPTIONAL_KEYS = %w[
+    required
+    method
+    body
+    params
+    skip_condition
+    success_data
+  ].freeze
+
+  private
+
+  def validate_config
+    unless config.is_a?(Hash)
+      errors.add(:config, 'must be a hash')
+      return
+    end
+
+    unless config.key?('liquid_templates') && config['liquid_templates'].is_a?(Hash)
+      errors.add(:config, 'must include liquid_templates hash')
+      return
+    end
+
+    validate_required_keys
+    validate_no_extra_keys
+  end
+
+  def validate_required_keys
+    CONFIG_REQUIRED_KEYS.each do |key|
+      errors.add(:config, "must include #{key}") unless config['liquid_templates'].key?(key)
+    end
+  end
+
+  def validate_no_extra_keys
+    extra_keys = config['liquid_templates'].keys - (CONFIG_REQUIRED_KEYS + CONFIG_OPTIONAL_KEYS)
+    extra_keys.each { |key| errors.add(:config, "unexpected key in liquid_templates: #{key}") }
+  end
 end
--- spec/models/workflow_step_spec.rb
+++ spec/models/workflow_step_spec.rb
@@ -1,5 +1,75 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe WorkflowStep, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#initialize' do
+    let(:step) { create(:workflow_step) }
+
+    it 'creates a new WorkflowStep with valid attributes' do
+      expect(step).to be_valid
+      expect(step.name).to be_a(String)
+      expect(step.order).to be_a(Integer)
+      expect(step.config).to be_a(Hash)
+    end
+
+    it 'validates presence of name' do
+      step.name = nil
+      expect(step).not_to be_valid
+      expect(step.errors[:name]).to include("can't be blank")
+    end
+
+    it 'validates presence of order' do
+      step.order = nil
+      expect(step).not_to be_valid
+      expect(step.errors[:order]).to include("can't be blank")
+    end
+
+    it 'validates order is a positive integer' do
+      step.order = -1
+      expect(step).not_to be_valid
+      expect(step.errors[:order]).to include('must be greater than 0')
+    end
+  end
+
+  describe '#config' do
+    it 'validates config is present and is a hash' do
+      step = build(:workflow_step, config: 'not a hash')
+      expect(step).not_to be_valid
+      expect(step.errors[:config]).to include('must be a hash')
+
+      step = build(:workflow_step, config: nil)
+      expect(step).not_to be_valid
+      expect(step.errors[:config]).to include("can't be blank")
+
+      step = build(:workflow_step, config: {})
+      expect(step).not_to be_valid
+      expect(step.errors[:config]).to include("can't be blank")
+    end
+
+    it 'validates liquid_templates is present in config' do
+      step = build(:workflow_step, config: { foo: 'bar' })
+      expect(step).not_to be_valid
+      expect(step.errors[:config]).to include('must include liquid_templates hash')
+    end
+
+    it 'validates required liquid_template fields' do
+      step = build(:workflow_step, config: { 'liquid_templates' => {} })
+      expect(step).not_to be_valid
+      expect(step.errors[:config]).to include('must include name')
+      expect(step.errors[:config]).to include('must include url')
+    end
+
+    it 'checks for unsupported keys in liquid_templates' do
+      step = build(:workflow_step, config: {
+                     'liquid_templates' => {
+                       'name' => '1',
+                       'url' => '{{base_url}}/customers/lookup.json?reference={{row.customer_reference}}',
+                       'unsupported_key' => 'value'
+                     }
+                   })
+      expect(step).not_to be_valid
+      expect(step.errors[:config]).to include('unexpected key in liquid_templates: unsupported_key')
+    end
+  end
 end
