d9a04f2af3db74fe57970a08c11909b0d930fa90
Luke <imlukedewitt@gmail.com>
2025-05-26 09:48:48 -0500

    Add WorkflowExecution model associations and validations

--- app/models/workflow_execution.rb
+++ app/models/workflow_execution.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
+##
+# WorkflowExecution model represents a specific run of a Workflow
+# Also known as "An Import" or "A Run"
 class WorkflowExecution < ApplicationRecord
+  belongs_to :workflow
+  belongs_to :data_source
+  has_many :rows, dependent: :destroy
+  has_many :batches, through: :rows
+
+  validates :workflow, presence: true
+  validates :data_source, presence: true
 end
