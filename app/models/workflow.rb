68fe560a5f3074757a35e8377650e46c29f7282f
Luke <imlukedewitt@gmail.com>
2025-05-26 09:48:06 -0500

    Enhance Workflow model with validations and execution helpers

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -2,6 +2,15 @@
 
 ##
 # Workflow model
+# Contains a collection of Steps to be executed in sequence
 class Workflow < ApplicationRecord
-  has_many :steps
+  has_many :steps, dependent: :destroy
+  has_many :workflow_executions, dependent: :restrict_with_error
+
+  validates :name, presence: true
+
+  # Helper for creating a workflow execution
+  def create_execution(data_source)
+    workflow_executions.create(data_source: data_source)
+  end
 end
