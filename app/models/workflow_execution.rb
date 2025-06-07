240d6bc80ca37956b65f01c923a6d86165336b65
Luke <imlukedewitt@gmail.com>
2025-06-07 12:36:30 -0500

    Include Executable concern in WorkflowExecution model

--- app/models/workflow_execution.rb
+++ app/models/workflow_execution.rb
@@ -3,6 +3,8 @@
 ##
 # WorkflowExecution model represents a specific run of a Workflow
 class WorkflowExecution < ApplicationRecord
+  include Executable
+
   belongs_to :workflow
   belongs_to :data_source
   has_many :rows, dependent: :destroy
