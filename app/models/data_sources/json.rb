5903b4ce1d7946de4665395c2e3953b107b075d6
Luke <imlukedewitt@gmail.com>
2025-08-03 19:46:33 -0500

    Refactor data source loading to simplify row creation
    Remove options parameter and workflow_execution requirement from
    CSV/JSON loaders Extract common row creation logic into build_row method
    Batch save rows at end of processing instead of creating individually

--- app/models/data_sources/json.rb
+++ app/models/data_sources/json.rb
@@ -6,54 +6,36 @@ module DataSources
   ##
   # JsonData model represents a JSON data source for workflow execution
   class Json < DataSource
-    def load_from_file_path(source, options = {})
-      process_json(File.read(source), options)
+    def load_from_file_path(source)
+      process_json(File.read(source))
     end
 
-    def load_from_uploaded_file(source, options = {})
-      process_json(source.read, options)
+    def load_from_uploaded_file(source)
+      process_json(source.read)
     end
 
-    def load_from_string(source, options = {})
-      process_json(source, options)
+    def load_from_string(source)
+      process_json(source)
     end
 
-    def load_from_stream(source, options = {})
-      process_json(source.read, options)
+    def load_from_stream(source)
+      process_json(source.read)
     end
 
     private
 
-    def process_json(content, options = {})
-      workflow_execution = options[:workflow_execution]
-      raise ArgumentError, 'workflow_execution is required' unless workflow_execution
-
+    def process_json(content)
       json_data = JSON.parse(content)
       rows_data = json_data.is_a?(Array) ? json_data : [json_data]
 
-      create_rows(rows_data, workflow_execution)
+      rows_data.each_with_index do |row_data, index|
+        build_row(row_data, index)
+      end
 
+      save_rows!
       self
     end
 
-    def create_rows(rows_data, workflow_execution)
-      rows_data.each_with_index do |row_data, index|
-        source_index = index
 
-        if row_data.is_a?(Hash) && (row_data.key?('source_index') || row_data.key?(:source_index))
-          source_index = row_data['source_index'] || row_data[:source_index]
-
-          row_data = row_data.dup
-          row_data.delete('source_index')
-          row_data.delete(:source_index)
-        end
-
-        rows.create!(
-          data: row_data,
-          workflow_execution: workflow_execution,
-          source_index: source_index
-        )
-      end
-    end
   end
 end
