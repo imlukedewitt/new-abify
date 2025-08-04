d991990d3a61310c5d9ebc6b87d32a0fd5587766
Luke <imlukedewitt@gmail.com>
2025-08-04 00:28:56 -0500

    Add input validation and error handling to DataSources::Builder
    Raise InvalidSourceError for invalid inputs and wrap all errors in this
    custom error class. Also add frozen_string_literal to
    workflow_execution_spec.

--- app/services/data_sources/builder.rb
+++ app/services/data_sources/builder.rb
@@ -5,12 +5,21 @@ module DataSources
   # It will take a source and a type and return a DataSource.
   # It will also set the name of the DataSource to the original filename if it exists.
   class Builder
+    class InvalidSourceError < StandardError; end
+
     def self.call(source:, type: 'csv')
+      unless source.is_a?(ActionDispatch::Http::UploadedFile) || source.is_a?(Rack::Test::UploadedFile)
+        raise InvalidSourceError,
+              "Source must be a file upload"
+      end
+
       "DataSources::#{type.camelize}".constantize.new(source: source).tap do |ds|
         ds.name = File.basename(source.original_filename) if source.respond_to?(:original_filename)
         ds.save!
         ds.load_data
       end
+    rescue StandardError => e
+      raise InvalidSourceError, e.message
     end
   end
 end
