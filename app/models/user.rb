589ccaec1213e00d3b41d5cde7a21ab4b7bab231
Luke <imlukedewitt@gmail.com>
2025-12-22 02:13:02 -0600

    users have api_tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
+  after_initialize :set_api_token, if: :new_record?
+
   has_many :connections, dependent: :destroy
+
+  validates :api_token, uniqueness: true
+
+  attribute :api_token, :string
+  encrypts :api_token, deterministic: true
+
+  private
+
+  def set_api_token
+    self.api_token ||= SecureRandom.hex(20)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'callbacks' do
+    it 'sets api_token for new records' do
+      user = build(:user)
+      expect(user.api_token).to be_present
+      expect(user.api_token).to be_a(String)
+      expect(user.api_token.length).to eq(40)
+    end
+  end
+
+  describe 'validations' do
+    let(:token) { 'the_token' }
+    let!(:user) { create(:user, api_token: token) }
+    let(:user2) { build(:user, api_token: token) }
+    it 'validates api_token is unique' do
+      expect(user2).not_to be_valid
+      expect(user2.errors[:api_token]).to include('has already been taken')
+    end
+  end
 end
