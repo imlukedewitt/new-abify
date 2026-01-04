e5b4bff32f2e7ab58b376764aa3c3218a11e18bc
Luke <imlukedewitt@gmail.com>
2026-01-03 18:03:57 -0600

    add some html handling to data source controller

--- app/controllers/data_sources_controller.rb
+++ app/controllers/data_sources_controller.rb
@@ -2,11 +2,21 @@
 
 # .
 class DataSourcesController < ApplicationController
+  def new
+    @data_source = DataSource.new
+  end
+
   def create
     data_source = DataSources::Builder.call(source: params[:source])
-    render json: { data_source_id: data_source.id }, status: :created
+    respond_to do |format|
+      format.json { render json: { data_source_id: data_source.id }, status: :created }
+      format.html { redirect_to data_source_path(data_source), notice: 'Data source created successfully.' }
+    end
   rescue DataSources::Builder::InvalidSourceError => e
-    render json: { error: "Invalid data source: #{e.message}" }, status: :bad_request
+    respond_to do |format|
+      format.json { render json: { error: "Invalid data source: #{e.message}" }, status: :bad_request }
+      format.html { redirect_to new_data_source_path, alert: "Invalid data source: #{e.message}" }
+    end
   end
 
   def index
