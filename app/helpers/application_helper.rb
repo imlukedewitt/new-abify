f695b86160d32026ff0b8a29a389172003a0dd57
Luke <imlukedewitt@gmail.com>
2026-01-03 23:33:04 -0600

    auto update navbar active link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def active_menu_item
+    case controller_name
+    when 'data_sources'
+      :data_sources
+    when 'workflows'
+      :workflows
+    when 'logs'
+      :logs
+    else
+      nil
+    end
+  end
 end
